6964b35a9b1f5d2a706ede5e011c4b83ad5ece14
kderune <91871014+xushabanana0123@users.noreply.github.com>
2022-11-16 22:23:58 +0000

    uzd. 6

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-saraksts = [int(sk) for sk in input("Ievadi skaitius, atdalot tos ar atstarpi:").split()]
-for i in range(len(saraksts)-1):
-  if saraksts[i] == saraksts[i + 1]:
-    print(saraksts[i], saraksts[i + 1])
-#tests: 2 2 4 4 8 9
-#izvade: 2 2; 4 4
+skaitli = [int(skaitli) for skaitli in input("levadi skaitlus, atdalot tos ar atstarpi:") .split()]
+lielIndex = 0
+for i in range(1, len(skaitli)):
+  if skaitli[i] > skaitli[lielIndex]:
+    lielIndex = i
+print("Lielakais skaitlis:", skaitli[lielIndex],
+"ar indeksu", lielIndex)
+#tests: 2 4 7 9
+#izvade: Lielakais skaitlis: 9 ar indeksu 3
